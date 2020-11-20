184294444200962c2bf3fb27973a5ce573966026
Martinique <me@martidolce.com>
2020-11-20 07:14:15 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+# frozen_string_literal: true
+
+require 'pry'
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# dog.rb
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+    puts @@all.map(&:name)
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+end
